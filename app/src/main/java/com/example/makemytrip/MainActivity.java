a7d97437bd71d89c3535b905e76e8574679cf769
Yash Ainapure <ainapureyash2@gmail.com>
2023-10-12 18:45:29 +0530

    login-page part 1

--- app/src/main/java/com/example/makemytrip/MainActivity.java
+++ app/src/main/java/com/example/makemytrip/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.makemytrip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginbtn;
+    EditText usernameEdt,passwordEdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameEdt=(EditText) findViewById(R.id.usernameedt);
+        passwordEdt=(EditText) findViewById(R.id.passwordedt);
+        loginbtn=(Button) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName=usernameEdt.getText().toString();
+                String pass=passwordEdt.getText().toString();
+                if(userName.length()<6 || pass.length()<6){
+                    Toast.makeText(MainActivity.this, "username and password length must be greater than 6", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "username : "+userName+" password : "+pass, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
