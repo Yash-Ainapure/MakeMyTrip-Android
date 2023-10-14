a981154a350048c1370cbeb6f03f5397fef7cc50
Yash Ainapure <ainapureyash2@gmail.com>
2023-10-14 11:28:47 +0530

    login page firebase connection

--- app/src/main/java/com/example/makemytrip/MainActivity.java
+++ app/src/main/java/com/example/makemytrip/MainActivity.java
@@ -1,48 +1,65 @@
 package com.example.makemytrip;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+
+
+import com.google.firebase.FirebaseApp;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button loginbtn;
     EditText usernameEdt,passwordEdt;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialise firebase
+        FirebaseApp.initializeApp(this);
+        mAuth=FirebaseAuth.getInstance();
+
         usernameEdt=(EditText) findViewById(R.id.usernameedt);
         passwordEdt=(EditText) findViewById(R.id.passwordedt);
         loginbtn=(Button) findViewById(R.id.loginbtn);
 
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Get the entered username and password
-                String enteredUsername = usernameEdt.getText().toString();
-                String enteredPassword = passwordEdt.getText().toString();
+        loginbtn.setOnClickListener(view -> {
+            // Get the entered username and password
+            String username = usernameEdt.getText().toString();
+            String password = passwordEdt.getText().toString();
 
-                // Check if the entered credentials are valid
-                if (enteredUsername.equals("harsh") && enteredPassword.equals("123456")) {
-                    // If valid, start the next activity
-                    Intent stm = new Intent(MainActivity.this, home.class);
-                    startActivity(stm);
-                } else {
-                    // If invalid, show a message or handle it as needed
-                    // For simplicity, you can display a toast message
-                    // indicating that the login failed
-                    // You may want to use a more secure authentication method
-                    // in a real-world scenario
-                    Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
-                }
+
+            //email: harsh@gmail.com
+            //password : 123456
+
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+                return;
             }
+            // Sign in with email and password
+            mAuth.signInWithEmailAndPassword(username,password)
+                    .addOnCompleteListener(MainActivity.this, task -> {
+                        if (task.isSuccessful()) {
+                            // If valid, start the next activity
+                            Intent stm = new Intent(MainActivity.this, home.class);
+                            startActivity(stm);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                        }
+                    });
         });
     }
 }
