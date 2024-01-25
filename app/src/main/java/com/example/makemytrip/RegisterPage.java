507c81429bb8506d68197016777d1749f57d76ae
Yash-Ainapure <ainapureyash2@gmail.com>
2024-01-25 11:55:54 +0530

    register info saved

--- app/src/main/java/com/example/makemytrip/RegisterPage.java
+++ app/src/main/java/com/example/makemytrip/RegisterPage.java
@@ -15,12 +15,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterPage extends AppCompatActivity {
 
-    EditText email,password;
+    EditText email,password,confirmpassword,firstName,lastName,mobileNo;
     TextView loginpage;
     Button register;
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,40 +35,64 @@ public class RegisterPage extends AppCompatActivity {
 
         email = findViewById(R.id.editTextEmail);
         password = findViewById(R.id.editTextPassword);
+        confirmpassword = findViewById(R.id.ConfirmPassword);
+        firstName = findViewById(R.id.RegFirstName);
+        lastName = findViewById(R.id.RegLastName);
+        mobileNo = findViewById(R.id.RegPhoneNo);
         register = findViewById(R.id.registerbtn);
 
         register.setOnClickListener(v -> {
             String emailid = email.getText().toString();
             String pass = password.getText().toString();
-            if(emailid.isEmpty()){
+            String confirmpass = confirmpassword.getText().toString();
+            String fname = firstName.getText().toString();
+            String lname = lastName.getText().toString();
+            String mobno = mobileNo.getText().toString();
+            if(fname.isEmpty()){
+                firstName.setError("Please enter first name");
+                firstName.requestFocus();
+            }
+            else if(lname.isEmpty()){
+                lastName.setError("Please enter last name");
+                lastName.requestFocus();
+            }
+            else if(mobno.isEmpty()){
+                mobileNo.setError("Please enter mobile number");
+                mobileNo.requestFocus();
+            }
+            else if(emailid.isEmpty() && pass.isEmpty() && confirmpass.isEmpty() && fname.isEmpty() && lname.isEmpty() && mobno.isEmpty()){
                 email.setError("Please enter email id");
                 email.requestFocus();
-            }
-            else if(pass.isEmpty()){
                 password.setError("Please enter password");
                 password.requestFocus();
+                confirmpassword.setError("Please enter confirm password");
+                confirmpassword.requestFocus();
+                firstName.setError("Please enter first name");
+                firstName.requestFocus();
+                lastName.setError("Please enter last name");
+                lastName.requestFocus();
+                mobileNo.setError("Please enter mobile number");
+                mobileNo.requestFocus();
             }
-            else if(emailid.isEmpty() && pass.isEmpty()){
-                email.setError("Please enter email id");
-                email.requestFocus();
-                password.setError("Please enter password");
-                password.requestFocus();
+            else if(!pass.equals(confirmpass)){
+                Toast.makeText(RegisterPage.this,"Password and Confirm Password are not same",Toast.LENGTH_SHORT).show();
             }
-            else if(!(emailid.isEmpty() && pass.isEmpty())){
+            else{
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailid,pass).addOnCompleteListener(RegisterPage.this, task -> {
                     if(!task.isSuccessful()){
                         Toast.makeText(RegisterPage.this,"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
                     }
                     else{
+
+                        saveAdditionalUserInfo(task.getResult().getUser().getUid(),fname, lname, mobno,emailid);
+
                         startActivity(new Intent(RegisterPage.this, MainActivity.class));
                         Toast.makeText(this, "registered successfully,now you can login with the credentials", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
-            else{
-                Toast.makeText(RegisterPage.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
-            }
         });
+
         loginpage = findViewById(R.id.loginbtnreg);
         loginpage.setOnClickListener(view -> {
             Intent stm = new Intent(RegisterPage.this, MainActivity.class);
@@ -95,5 +123,17 @@ public class RegisterPage extends AppCompatActivity {
                 startIndexPP, endIndexPP, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         // Set the modified text to the TextView
         tcTextView.setText(spannableStringBuilder);
+    }
+    private void saveAdditionalUserInfo(String userId, String firstName, String lastName, String phoneNumber, String email) {
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(userId).child("userInfo");
+        UserInfo userInfo = new UserInfo(userId,firstName,lastName,phoneNumber,email);
+        databaseReference.setValue(userInfo);
+
+
+//        DatabaseReference userRef = usersRef.child(userId);
+//        userRef.child("firstName").setValue(firstName);
+//        userRef.child("lastName").setValue(lastName);
+//        userRef.child("phoneNumber").setValue(phoneNumber);
     }
 }
---
+++ app/src/main/java/com/example/makemytrip/UserInfo.java
@@ -0,0 +1,55 @@
+package com.example.makemytrip;
+
+public class UserInfo {
+    String userId,firstName,lastName, phoneNumber, email;
+
+    public UserInfo() {
+    }
+    public UserInfo(String userId, String firstName, String lastName, String phoneNumber, String email) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
